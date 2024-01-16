4b0ea3b6b4f178c2bee43c811b3290afa75d4582
taithang <taithangdn99@gmail.com>
2024-01-16 08:51:52 +0700

    update UI

---
+++ trafficproject/src/components/InfoTable.jsx
@@ -0,0 +1,200 @@
+function InfoTable() {
+  return (
+    <div>
+      <center>
+        <h3 className="css-1oevxvn">
+          Ph át hi ện <span style={{ color: "red" }}>1</span>l ỗi vi ph ạm,
+          trong đó:
+        </h3>
+        <div className>
+          <button
+            className="css-tt"
+            type="button"
+            style={{ borderColor: "red", color: "red" }}
+          >
+            0 CH ƯA X ỬPH ẠT
+          </button>
+          <button
+            className="css-tt"
+            type="button"
+            style={{ marginLeft: "10px", borderColor: "green", color: "green" }}
+          >
+            1 ĐÃX ỬPH ẠT
+          </button>
+        </div>
+      </center>
+      <br />
+      <div className="body_table">
+        <table className="css_table">
+          <tbody className="css_td_left">
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>
+                  <b style={{ fontWeight: 550, fontSize: "18px" }}>
+                    Bi ển s ố:
+                  </b>
+                </b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  <b style={{ fontWeight: 550, fontSize: "18px" }}>
+                    43A-599.41
+                  </b>
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>M àu bi ển:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  N ền m ầu tr ắng, ch ữv às ốm àu đen
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>Lo ại ph ương ti ện:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>Ôt ô</b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>Th ời gian vi ph ạm:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>13:43, 09/11/2023</b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>Địa đi ểm vi ph ạm:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  S ốnh à207 Tr ường Chinh, ĐàN ẵng (SP1N061), Ph ường H òa Kh
+                  ê, Qu ận Thanh Kh ê, Th ành ph ốĐàN ẵng
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>H ành vi vi ph ạm:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  12321.5.3.a.01.Đi ều khi ển xe ch ạy qu át ốc độquy định t ừ05
+                  km/h đến d ưới 10 km/h
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>Tr ạng th ái:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  <button
+                    className="css-tt"
+                    type="button"
+                    style={{ borderColor: "green", color: "green" }}
+                  >
+                    Đãx ửph ạt
+                  </button>
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left">
+                <b style={{ fontWeight: 550 }}>Đơn v ịph át hi ện vi ph ạm:</b>
+              </td>
+              <td className="row_right">
+                <b style={{ fontWeight: 400 }}>
+                  Đội Tuy ên truy ền, ĐTGQTN v àXLVP - PH ÒNG C ẢNH S ÁT GIAO TH
+                  ÔNG ĐÀN ẴNG
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_left" colSpan={2}>
+                <b style={{ fontWeight: 550 }}>N ơi gi ải quy ết v ụvi ệc:</b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_right" colSpan={2}>
+                <b style={{ fontWeight: 400 }}>
+                  1. Đội Tuy ên truy ền, ĐTGQTN v àXLVP - PH ÒNG C ẢNH S ÁT GIAO
+                  TH ÔNG ĐÀN ẴNG
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_right" colSpan={2}>
+                <b style={{ fontWeight: 400 }}>
+                  Địa ch ỉ: Giao l ộTr ần L ê- V õAn Ninh, H òa Xu ân, C ẩm L ệ,
+                  ĐàN ẵng
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_right" colSpan={2}>
+                <b style={{ fontWeight: 400 }}>
+                  2. Đội C ảnh s át giao th ông, Tr ật t ự, C ông an qu ận C ẩm
+                  L ệ- C ông an qu ận C ẩm L ệ- Th ành ph ốĐàN ẵng
+                </b>
+              </td>
+            </tr>
+            <tr className="td_left">
+              <td className="row_right" colSpan={2}>
+                <b style={{ fontWeight: 400 }}>Địa ch ỉ: qu ận C ẩm L ệ</b>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <center>
+        <p>
+          <i style={{ fontSize: "16px", lineHeight: "18px!important" }}>
+            C ảm ơn qu ýv ịđãs ửd ụng ứng d ụng, Xin qu ýv ịh ãy d ành ch út th
+            ời gian đểủng h ộT ác gi ảb ằng c ách nh ấn n út Chia s ẻỨng d ụng l
+            ên Facebook, Zalo...đểm ọi ng ười c ùng bi ết đến.
+          </i>
+        </p>
+        <p>
+          <a
+            className="css-tt"
+            href="https://www.facebook.com/sharer/sharer.php?u=https://phatnguoixe.com"
+            type="button"
+            style={{
+              borderColor: "green",
+              color: "green",
+              padding: "5px 10px",
+            }}
+          >
+            ↑CHIA S ẺFACEBOOK
+          </a>
+          <br />
+        </p>
+        <hr />
+        Ngu ồn{" "}
+        <b style={{ fontWeight: 550 }}>
+          'C ổng th ông tin đi ện t ửC ục C ảnh s át giao th ông'
+        </b>
+        <br />
+        Ph ần m ềm tra c ứu ph ạt ngu ội <br />
+        Phi ên b ản 2023
+      </center>
+      <div className="cttdt">
+        <b>Nguồn: Cổng thông tin điện tử Cục Cảnh sát giao thông</b>
+        <a href="https://xuphat.com/tin-tuc-giao-thong-cap-nhat-lien-tuc-hang-ngay.html">
+          <p className="mp">Tin Tức Giao Thông</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default InfoTable;
